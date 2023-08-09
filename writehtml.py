df35e77aa4e75abc3c601d68dba15fc0272c03ad
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-09 16:27:05 +0100

    Create writehtml.py

---
+++ writehtml.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+import sys
+
+ip_address = urlopen("https://ipinfo.io/ip").read().decode('utf-8')
+port = sys.argv[1]
+
+with open("index.html", "w") as f:
+  f.write(f'''
+<iframe src="{ip_address}:{port}" title="Flask App" id="flask_app"></iframe>
+<script>
+let elem = document.getElementById("flask_app");
+elem.setAttribute("width", window.innerWidth);
+elem.setAttribute("height", window.innerHeight);
+</script>
+  ''')
